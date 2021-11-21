fc8309a316782fa8a2cc94b59bffa5aa09600cf4
gi0cann <gionnec@gmail.com>
2021-11-21 04:16:31 +0000

    fixes some issues and add del command

--- moviebot.py
+++ moviebot.py
@@ -26,32 +26,53 @@ fd.close()
 
 members = ''
 bot = commands.Bot(command_prefix='!')
+movie_help = f"""!movie add <movie name>
+!movie list
+!movie del <movie name>
+!movie random
+"""
 
-@bot.command()
-async def hello(ctx, arg1):
-    await ctx.send(f"hello {arg1}")
 
-@bot.command()
+@bot.command(help=movie_help)
 async def movie(ctx, *args):
-    await ctx.send(f"{ctx.author}")
+    current_user = ctx.author.name + "#" + ctx.author.discriminator
+    print(current_user, current_user == "gi0cann#0899", ctx.author.mention)
     cmd = args[0]
     print(moviesdb, f"start {cmd}")
     if cmd  == "add":
         movie = " ".join(args[1:])
         print(f"movie: {movie}")
-        if movie not in moviesdb:
+        if movie not in moviesdb.get("movies"):
             moviesdb.get("movies").append(movie)
             print(moviesdb, "post add")
             await ctx.send(f"{movie} added to movie list")
-        fd = open("movies.json", "w")
-        json.dump(moviesdb, fd)
-        fd.close()
+            fd = open("movies.json", "w")
+            json.dump(moviesdb, fd)
+            fd.close()
+            await ctx.send(f"{ctx.author} added {movie} to movie list")
 
     if cmd == "list":
         await ctx.send(f"{', '.join(moviesdb.get('movies'))}")
 
     if cmd == "random":
-        movie = moviesdb.get("movies")[random.randint(0, len(moviesdb)-1)]
+        shuffled = moviesdb.get("movies")
+        random.shuffle(shuffled)
+        movie = shuffled[random.randint(0, len(moviesdb.get("movies"))-1)]
         await ctx.send(f"{movie}")
+
+    if cmd == "del":
+        if current_user == "gi0cann#0889":
+            movie = " ".join(args[1:])
+            if movie in moviesdb.get("movies"):
+                moviesdb["movies"].remove(movie)
+                fd = open("movies.json", "w")
+                json.dump(moviesdb, fd)
+                fd.close()
+                await ctx.send(f"{ctx.author} removed {movie} from movie list")
+            else:
+                newline = "\n"
+                await ctx.send(f"{movie} not in movie list. Try one of the following {newline.join(moviesdb.get('movies'))}")
+        else:
+            await ctx.send(f"User {current_user} is not allowed to remove movies from movie list. Better luck next time")
 
 bot.run(TOKEN)
