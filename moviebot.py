1a35d07ff156cfd2890e0df995de618f8c0cf0e0
Gionne Cannister <gionnec@gmail.com>
2021-11-21 00:18:53 -0500

    Switch logic to use bot.group decorator to implement sub-commands instead of ugly if statements.

--- moviebot.py
+++ moviebot.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+NEWLINE = "\n"
 
 if os.path.exists("movies.json"):
     fd = open("movies.json", "r")
@@ -26,53 +27,59 @@ fd.close()
 
 members = ''
 bot = commands.Bot(command_prefix='!')
-movie_help = f"""!movie add <movie name>
-!movie list
-!movie del <movie name>
-!movie random
-"""
 
+@bot.group()
+async def movie(ctx):
+    """Add, list, removes, and randomly selects moive from movie list"""
+    if ctx.invoked_subcommand is None:
+        await ctx.send("Missing subcommand")
 
-@bot.command(help=movie_help)
-async def movie(ctx, *args):
-    current_user = ctx.author.name + "#" + ctx.author.discriminator
-    print(current_user, current_user == "gi0cann#0899", ctx.author.mention)
-    cmd = args[0]
-    print(moviesdb, f"start {cmd}")
-    if cmd  == "add":
-        movie = " ".join(args[1:])
-        print(f"movie: {movie}")
-        if movie not in moviesdb.get("movies"):
-            moviesdb.get("movies").append(movie)
-            print(moviesdb, "post add")
-            await ctx.send(f"{movie} added to movie list")
+@movie.command(name="list")
+async def list_movies(ctx):
+    """List movies added to movie list."""
+    await ctx.send(f"```{NEWLINE.join(moviesdb.get('movies'))}```")
+
+@movie.command(name="add")
+async def add_movie(ctx, *movie_name):
+    """Add movie to movie list."""
+    movie = " ".join(movie_name)
+    print(f"movie: {movie}")
+    if movie not in moviesdb.get("movies"):
+        moviesdb.get("movies").append(movie)
+        await ctx.send(f"{movie} added to movie list")
+        fd = open("movies.json", "w")
+        json.dump(moviesdb, fd)
+        fd.close()
+        await ctx.send(f"{ctx.author} added {movie} to movie list")
+
+@movie.command(name="del")
+async def delete_movie(ctx, *movie_name):
+    """Remove a movie from movie list."""
+    print(ctx.author.id)
+    if ctx.author.id == 159128365079592960:
+        movie = " ".join(movie_name)
+        if movie in moviesdb.get("movies"):
+            moviesdb["movies"].remove(movie)
             fd = open("movies.json", "w")
             json.dump(moviesdb, fd)
             fd.close()
-            await ctx.send(f"{ctx.author} added {movie} to movie list")
-
-    if cmd == "list":
-        await ctx.send(f"{', '.join(moviesdb.get('movies'))}")
-
-    if cmd == "random":
-        shuffled = moviesdb.get("movies")
-        random.shuffle(shuffled)
-        movie = shuffled[random.randint(0, len(moviesdb.get("movies"))-1)]
-        await ctx.send(f"{movie}")
-
-    if cmd == "del":
-        if current_user == "gi0cann#0889":
-            movie = " ".join(args[1:])
-            if movie in moviesdb.get("movies"):
-                moviesdb["movies"].remove(movie)
-                fd = open("movies.json", "w")
-                json.dump(moviesdb, fd)
-                fd.close()
-                await ctx.send(f"{ctx.author} removed {movie} from movie list")
-            else:
-                newline = "\n"
-                await ctx.send(f"{movie} not in movie list. Try one of the following {newline.join(moviesdb.get('movies'))}")
+            await ctx.send(f"{ctx.author} removed {movie} from movie list")
         else:
-            await ctx.send(f"User {current_user} is not allowed to remove movies from movie list. Better luck next time")
+            await ctx.send(
+                f"{movie} not in movie list. Try one of the following: ```{NEWLINE.join(moviesdb.get('movies'))}```"
+            )
+    else:
+        await ctx.send(
+            f"Nice try asshole. User {ctx.author} is not allowed to remove movies from movie list. Better luck next time"
+        )
+
+@movie.command(name="random")
+async def pick_random_movie(ctx):
+    """Pick a random movie from movie list."""
+    shuffled = moviesdb.get("movies")
+    random.shuffle(shuffled)
+    random.shuffle(shuffled)
+    movie = shuffled[random.randint(0, len(moviesdb.get("movies"))-1)]
+    await ctx.send(f"{movie}")
 
 bot.run(TOKEN)
