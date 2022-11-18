56cf038c5f891004870f18475844f6b2ea1f192e
gi0cann <gionnec@gmail.com>
2022-11-18 04:47:18 +0000

    Updates code to support the newly required intents.

--- moviebot.py
+++ moviebot.py
@@ -7,6 +7,10 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+#intents = discord.Intents(message_content=True)
+intents = discord.Intents.default()
+intents.message_content = True
+
 def clean_input(in_str):
     in_split = in_str.split(" ")
     result = list()
@@ -34,7 +38,7 @@ else:
 fd.close()
 
 members = ''
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.group()
 async def movie(ctx):
