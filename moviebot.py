17e3b799d5be1803be1fe3e803f65e097493132a
gi0cann <gionnec@gmail.com>
2022-11-18 08:23:01 +0000

    Adds audio play support

--- moviebot.py
+++ moviebot.py
@@ -2,6 +2,7 @@ import os
 import random
 import json
 import os.path
+import time
 
 import discord
 from discord.ext import commands
@@ -10,6 +11,7 @@ from dotenv import load_dotenv
 #intents = discord.Intents(message_content=True)
 intents = discord.Intents.default()
 intents.message_content = True
+intents.messages = True
 
 def clean_input(in_str):
     in_split = in_str.split(" ")
@@ -38,11 +40,18 @@ else:
 fd.close()
 
 members = ''
-bot = commands.Bot(command_prefix='!', intents=intents)
+help_command = commands.DefaultHelpCommand(dm_help=True)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=help_command)
 
 @bot.group()
 async def movie(ctx):
     """Watch party movie list commands"""
+    if ctx.invoked_subcommand is None:
+        await ctx.send("Missing subcommand")
+
+@bot.group()
+async def audio(ctx):
+    """Audio commands"""
     if ctx.invoked_subcommand is None:
         await ctx.send("Missing subcommand")
 
@@ -97,5 +106,65 @@ async def pick_random_movie(ctx):
     random.shuffle(shuffled)
     movie = shuffled[random.randint(0, len(moviesdb.get("movies"))-1)]
     await ctx.send(f"{movie}")
+
+async def play_audio(source, ctx, executable="/usr/bin/ffmpeg"):
+    """play audio file"""
+    voice_channel = ctx.author.voice.channel
+    channel = None
+    if voice_channel != None:
+        channel = voice_channel.name
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio(executable=executable, source=source))
+        while vc.is_playing():
+            time.sleep(.1)
+        await vc.disconnect()
+    else:
+        await ctx.send(str(ctx.author.name) + "you are not in a voice channel. You dumb fuck. And I'm supposed to be the Jerry.")
+    await ctx.message.delete()
+
+@audio.command(name="evan")
+async def play_evan(ctx):
+    """Play evan audio"""
+    await play_audio(source="audio/evan_stfu.mp3", ctx=ctx)
+
+@audio.command(name="hothothot")
+async def play_hothothot(ctx):
+    """Play hothothot audio"""
+    await play_audio(source="audio/hot hot hot hot.mp3", ctx=ctx)
+
+@audio.command(name="kevdamn")
+async def play_kevdamn(ctx):
+    """Play kevin hart damn audio"""
+    await play_audio(source="audio/Kevin hart high damn.mp3", ctx=ctx)
+
+@audio.command(name="kevdamnlong")
+async def play_kevdamnlong(ctx):
+    """Play kevin hart damn long audio"""
+    await play_audio(source="audio/kevin hart high damn & im sorry.mp3", ctx=ctx)
+
+@audio.command(name="animemoan")
+async def play_animemoan(ctx):
+    """Play moan audio"""
+    await play_audio(source="audio/Anime Moan  Sound Effect.mp3", ctx=ctx)
+
+@audio.command(name="moan")
+async def play_moan(ctx):
+    """Play moan audio"""
+    await play_audio(source="audio/moan.mp3", ctx=ctx)
+
+@audio.command(name="notthatguy")
+async def play_notthatguy(ctx):
+    """Play Bill your not that guy audio"""
+    await play_audio(source="audio/Youre not that guy pal trust me youre not that guy SOUND EFFECT.mp3", ctx=ctx)
+
+@audio.command(name="perfect")
+async def play_perfect(ctx):
+    """Play Street figher perfect"""
+    await play_audio(source="audio/Perfect Street Fighter Sound Effect.mp3", ctx=ctx)
+
+@audio.command(name="peterlaugh")
+async def play_peterlaugh(ctx):
+    """Play Peter Grifin laugh"""
+    await play_audio(source="audio/peter griffin laugh.mp3", ctx=ctx)
 
 bot.run(TOKEN)
